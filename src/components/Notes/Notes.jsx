f264457360c685477acbc777cbaf0b4480f8a1f0
Mayank <mayankbudhiraja88@gmail.com>
2023-04-04 17:05:01 +0530

    added drag and drop functionality

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { Box, Grid } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 // Components
 import Form from "./Form";
@@ -19,6 +20,21 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 const Notes = () => {
   const { notes, setNotes } = useContext(DataContext);
 
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = reorder(notes, result.source.index, result.destination.index);
+    setNotes(items);
+  };
+
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem("noteskey"));
     if (items) setNotes(items);
@@ -29,15 +45,33 @@ const Notes = () => {
       <DrawerHeader />
       <Form />
       {notes.length > 0 ? (
-        <Grid container style={{ marginTop: "16px" }}>
-          {notes.map((note) => {
-            return (
-              <Grid item key={note.id}>
-                <Note note={note} />
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <Grid
+                container
+                style={{ marginTop: "16px" }}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {notes.map((note, index) => (
+                  <Draggable key={note.id} draggableId={note.id} index={index}>
+                    {(provided, snapshot) => (
+                      <Grid
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        item
+                      >
+                        <Note note={note} />
+                      </Grid>
+                    )}
+                  </Draggable>
+                ))}
               </Grid>
-            );
-          })}
-        </Grid>
+            )}
+          </Droppable>
+        </DragDropContext>
       ) : (
         <EmptyNotes
           Image={
